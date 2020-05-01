217613f7a80e68d7ea0031e5963d987a0c3e43c0
PabloBaiocchi <pablo.b.baiocchi@gmail.com>
2020-05-01 17:07:50 -0300

    make calls easier

--- src/data.py
+++ src/data.py
@@ -2,15 +2,18 @@ import gspread
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
+import pandas as pd
 
 def getSpreadsheetAuth(): 
   auth.authenticate_user()
   spreadsheetAuth=gspread.authorize(GoogleCredentials.get_application_default())
   return spreadsheetAuth
 
-def getSpreadsheet(spreadsheetAuth,spreadsheetUrl,sheetName):
+def getSpreadsheet(spreadsheetUrl,sheetName):
+    spreadsheetAuth=getSpreadsheetAuth()
     sheet=spreadsheetAuth.open_by_url(spreadsheetUrl)
     frame=get_as_dataframe(sheet.worksheet(sheetName))
     frame=frame.dropna(how='all')
     frame=frame.dropna(axis='columns',how='all')
     return frame
+
