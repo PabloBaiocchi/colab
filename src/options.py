d620fa77a7ff9d0fe40143b3bcbb9a02958c36c5
PabloBaiocchi <pablo.b.baiocchi@gmail.com>
2020-05-01 23:42:44 -0300

    more functions

--- src/options.py
+++ src/options.py
@@ -1,16 +1,26 @@
-def callProfit(strikePrice,optionPremium):
+def callProfitFunction(strikePrice,optionPremium):
   def profitFunction(spotPrice):
     if spotPrice>strikePrice:
       return spotPrice-strikePrice-optionPremium
     return -optionPremium
   return profitFunction
 
-def putProfit(strikePrice,optionPremium):
+def putProfitFunction(strikePrice,optionPremium):
   def profitFunction(spotPrice):
     if spotPrice<strikePrice:
       return strikePrice-spotPrice-optionPremium
     return -optionPremium 
   return profitFunction
+
+def callProfit(spotPrice,strikePrice,optionPremium):
+    if spotPrice>strikePrice:
+      return spotPrice-strikePrice-optionPremium
+    return -optionPremium
+
+def putProfit(spotPrice,strikePrice,optionPremium):
+    if spotPrice<strikePrice:
+      return strikePrice-spotPrice-optionPremium
+    return -optionPremium 
 
 def constant(constantValue):
     def constantFunction(spotPrice):
