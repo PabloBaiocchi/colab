a670f2928a5b24414b6d29e252b97f55665e5948
PabloBaiocchi <pablo.b.baiocchi@gmail.com>
2020-05-26 20:39:38 -0300

    add october expiration

--- src/config.py
+++ src/config.py
@@ -3,5 +3,6 @@ chromeDriverPath='/Users/pablo/Documents/chromedriver-chromeversion72'
 optionExpirationDates={
     'MY':'2020-5-15',
     'JU':'2020-6-19',
-    'AG':'2020-8-21'
+    'AG':'2020-8-21',
+    'OC':'2020-10-16'
 }
